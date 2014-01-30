899e37124d9bd9c6b2b6644d79683500abeff60d
HoggenMari <m.hoggenmueller@gmail.com>
2014-01-30 10:56:01 +0100

    first commit2

---
+++ src/Pavillon.java
@@ -0,0 +1,116 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+
+
+public class Pavillon {
+	
+	private final String IP_ADRESS;
+	private final int PORT;
+	private final byte ID;
+
+	private static byte port_map[] =
+		{1, 1, 1, 1, 2, 2, 2, 2,
+		 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
+		 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6,
+		 7, 7, 7, 7, 8, 8, 8, 8};
+	
+	ArrayList<Nozzle> nozzleList = new ArrayList<Nozzle>();
+	private InetAddress dest;
+	private MulticastSocket socket;
+
+	
+	public Pavillon(String ip, int i, int j) {
+		IP_ADRESS = ip;
+		PORT = i;
+		ID = (byte)j;
+		
+		try {
+			socket = new MulticastSocket();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void add(Node node){
+		for(int i=0; i<node.nozzleList.size(); i++){
+		nozzleList.add(node.nozzleList.get(i));
+		}
+	}
+
+	public void send() {
+		byte[] data = new byte[1472];
+		
+		int nozzle_count = 0;
+
+		for(int i=0; i<2; i++){
+		int dataIndex = 0;
+
+		data[dataIndex++] = 'Y';
+		data[dataIndex++] = 'T';
+		data[dataIndex++] = 'K';
+		data[dataIndex++] = 'J';
+		
+		data[dataIndex++] = (byte) ID;
+		data[dataIndex++] = 0;
+		data[dataIndex++] = 0x57;
+		data[dataIndex++] = 0x05;
+		
+		data[dataIndex++] = (byte) 1;
+		data[dataIndex++] = 0;
+		
+		int channel = i*2048;
+		
+		data[dataIndex++] = (byte) (channel & 0xff);
+		data[dataIndex++] = (byte) ((channel >> 8) & 0xff);
+
+		data[dataIndex++] = (byte) ((1458) & 0xff);
+		data[dataIndex++] = (byte) (((1458) >> 8) & 0xff);
+		
+		
+		do{
+			byte[] buffer = nozzleList.get(nozzle_count).data;
+			
+			//Now write the RGB-values
+			for(int j=0; j<buffer.length; j++) {
+				data[dataIndex++] = buffer[j];
+			}
+			
+			nozzle_count++;
+		}while(port_map[nozzle_count]==i+1);
+		
+		for(int j=dataIndex; j<1458; j++){
+			data[dataIndex++]= (byte) 40;
+		}
+				
+		DatagramPacket dp = new DatagramPacket(data, data.length);
+        dp.setPort(PORT);
+        try {
+			dest = InetAddress.getByName(IP_ADRESS);
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        dp.setAddress(dest);
+        
+        //p.delay(50);
+        
+        for(int k=0; k<data.length; k++){
+			System.out.println("DATA: "+i+" "+k+" "+data[k]);
+		}
+
+        try {
+          socket.send(dp);
+        } catch (IOException e) {
+        	
+        }
+		
+		}
+		
+	}
+}
---
+++ src/ProcessingMain.java
@@ -0,0 +1,265 @@
+
+import java.io.IOException;
+
+import controlP5.ControlP5;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PImage;
+
+public class ProcessingMain extends PApplet {
+	
+	/*Luminale
+	 * 
+	 */
+	
+	ControlP5 cp5;
+	
+	public static void main(String args[]) {
+	    PApplet.main(new String[] { "--present", "ProcessingMain" });
+	  }
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Node node1 = new Node(this, new int[]{0,1});
+	Node node2 = new Node(this, new int[]{0,1});
+	Node node3 = new Node(this, new int[]{0,1});
+	Node node4 = new Node(this, new int[]{0,1});
+	Node node5 = new Node(this, new int[]{0,1});
+	Node node6 = new Node(this, new int[]{0,1});
+	Node node7 = new Node(this, new int[]{0,1});
+
+	private PGraphics pg;
+
+
+	
+	int hue1=255;
+	int hue2=255;
+	int hue3=255;
+
+	private PImage img;
+
+	//Animation Stuff
+	private int xpos=0;
+	private int n=0;
+	private int num=8;
+	private int pixel_pos = -20;
+	private SpreadC[] sc=new SpreadC[num];
+	private int pixel_posy = -12;
+
+
+	private float pixel_x=0;
+
+	private final String IP = "224.1.1.1";
+	private final byte CONTROLLER_ID_1 = 4;
+	private final byte CONTROLLER_ID_2 = 4;
+	private Pavillon scp;
+
+
+
+
+
+
+	
+	public void setup() {
+		
+		size(1200,800);
+		
+		
+		//ControlP5
+		cp5 = new ControlP5(this);
+
+		cp5.addTextfield("IP")
+		   .setPosition(10,10)
+		   .setSize(50,15)
+		   .setFocus(true)
+		   .setColor(color(255,255,255))
+		   .setColorCaptionLabel(color(0,0,0))
+		   .setText(IP);
+		;
+		
+		cp5.addTextfield("ID-1")
+		   .setPosition(70,10)
+		   .setSize(20,15)
+		   .setFocus(true)
+		   .setColor(color(255,255,255))
+		   .setColorCaptionLabel(color(0,0,0))
+		   .setText(Integer.toString(CONTROLLER_ID_1));
+		;
+		
+		cp5.addTextfield("ID-2")
+		   .setPosition(100,10)
+		   .setSize(20,15)
+		   .setFocus(true)
+		   .setColor(color(255,255,255))
+		   .setColorCaptionLabel(color(0,0,0))
+		   .setText(Integer.toString(CONTROLLER_ID_2));
+		;
+		
+		cp5.addButton("OK")
+		   .setValue(0)
+		   .setPosition(130,10)
+		   .setSize(20,15)
+		;
+		
+		
+		for (int i=0; i<num; i++) {
+			    sc[i]=new SpreadC(this,3, 3, random(-1, 1), random(-1, 1), random(1, 1), 
+			    random(255), random(255), random(255));
+		}
+		
+		frameRate(50);
+		
+
+		pg = createGraphics(600, 400);
+		
+		int node1_leds[] = {75,60,75,60,60,75,60,75};
+		int node2_leds[] = {75,75,75,75,60,60,75,75,60,90};
+		int node3_leds[] = {75,75,75,60,90,75,75,60,75,75,75};
+		int node4_leds[] = {60,75,60,75,60,75,75,60};
+		int node5_leds[] = {60,90,75,60,75,75,75,90,75,75,60};
+		int node6_leds[] = {75,75,45,90,60,60,90,75,60};
+		int node7_leds[] = {75,75,75,75,60,60,60,60,90};
+
+
+		node1.add(node1_leds);
+		node2.add(node2_leds);
+		node3.add(node3_leds);
+		node4.add(node4_leds);
+		node5.add(node5_leds);
+		node6.add(node6_leds);
+		node7.add(node7_leds);
+		
+		String ip = "224.1.1.1";
+		scp = new Pavillon(ip, 5026, 4);
+		scp.add(node1);
+
+
+		/*m = new Movie(this,"/Users/mariushoggenmuller/Documents/BachelorArbeit/FinalLighterSketch/Videos/Neon Jogginghose.avi");
+		m.loop();*/
+				
+		/*img = loadImage("../Images/bg_small.png");
+		img.resize(12, 5);*/
+		
+
+	}
+
+	public void draw() {
+		  background(255);
+
+		  sc[0].update();
+		  sc[0].display(pg);
+		   
+		  int durchmesser=5;
+
+		  if (xpos > pg.width + durchmesser) {
+		    xpos = -durchmesser;
+		  }
+
+ 
+		  for(int i=0; i<node1.nozzleList.size(); i++){  
+			
+			System.out.println(i);
+			pg = node1.nozzleList.get(i).sysA;
+		    System.out.println(i);
+		   // PImage img1 = pg.get(0, 0, pg.width, pg.height);
+		    //image(img1, 0, 0);
+		    
+			pg.beginDraw();
+			pg.fill(0,0,0,40);
+			pg.rect(0,0,pg.width,pg.height);
+			
+			//Background-Gradient
+			for(int ix=0; ix<14; ix++){
+			pg.colorMode(HSB);
+			pg.fill(120+50+2*ix+10*i,200,150,255);
+			pg.noStroke();
+			pg.rect(ix, 0, 1, 5);
+			}
+			pg.colorMode(RGB);
+		    
+		  //New
+			for(int ii=0; ii<6; ii++){
+				pg.fill(255,20,25,20*ii);
+				pg.noStroke();
+				pg.rect(pixel_posy+ii, 0, 1, 5);
+			}
+			for(int ii=0; ii<6; ii++){
+				pg.fill(255,20,25,100-20*ii);
+				pg.noStroke();
+				pg.rect(pixel_posy+ii+6, 0, 1, 5);
+			}
+		    
+		    if(frameCount%5==0){
+		    pixel_x++;
+		    }
+		    
+		    if(pixel_x>5){
+		    	pixel_x=0;
+		    }
+			
+		        
+		    pg.endDraw();
+		    	    
+			pg = node1.nozzleList.get(4).sysB;
+		    pg.beginDraw();
+
+		    pg.fill(20*i, 20*i, 20*i, 255);
+		    pg.rect(0, 0, pg.width, pg.height);
+		    
+		    pg.endDraw();
+		    
+		    
+		    
+		   }
+		  
+		  
+		xpos = xpos+1; 
+		pixel_pos = pixel_pos + 1;
+		pixel_posy = pixel_posy + 1;
+
+			 
+		  if(frameCount%30==0){
+		  if(n>8){
+			  n=0;
+		  }else n++;
+		  }
+		  if (xpos > 20) {
+			    xpos = 0;
+			  }
+		  
+		if(frameCount%40==0){
+		  if (pixel_posy > 12) {
+			    pixel_posy = -12;
+			    //pixel_posy  += 1;
+			  }
+		}
+
+
+			node1.drawOnGui(10, 50);
+			node2.drawOnGui(150, 50);
+			node3.drawOnGui(300, 50);
+			node4.drawOnGui(450, 50);
+			node5.drawOnGui(600, 50);
+			node6.drawOnGui(750, 50);
+			node7.drawOnGui(900, 50);
+
+			scp.send();
+			/*try {
+				node1.sendNode(CONTROLLER_ID_1);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/		
+
+	}
+
+	//Called every time a new frame is available to read (Video-Background)
+	/*public void movieEvent(Movie m) {
+		m.read();
+	}*/
+
+
+
+}
